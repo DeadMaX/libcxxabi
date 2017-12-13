0ac05429102677efc6b70478fadfdc51d6afbb47
Alexandre Martins <deadmaxfr@gmail.com>
2017-12-14 00:58:54 +0100

    Rework abort message to be 100% C++ style

--- src/abort_message.h
+++ src/abort_message.h
@@ -12,16 +12,55 @@
 
 #include "cxxabi.h"
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include <vector>
+#include <string>
+#include <type_traits>
 
-_LIBCXXABI_HIDDEN _LIBCXXABI_NORETURN void
-abort_message(const char *format, ...) __attribute__((format(printf, 1, 2)));
+namespace utility
+{
+	template<typename T, typename = enable_if<!is_same<T, char>::value>::type>
+	typename decltype(to_string(T{})) stringify(T && t)
+	{
+		return to_string(forward<T>(t));
+	}
 
-#ifdef __cplusplus
+	template<typename T>
+	typename enable_if<is_same<T, char>::value, string>::type stringify(T && t)
+	{
+		return string(1, t);
+	}
+
+	template<typename T>
+	typename enable_if<!is_arithmetic<T>::value, string>::type stringify(T && t)
+	{
+		return string(forward<T>(t));
+	}
+
+	template<typename T, typename... Args>
+	void convert(vector<string> &tab, size_t i, T && v, Args... && args)
+	{
+		tab[i] = stringify(forward<T>(v));
+		convert(tab, i + 1, forward<Args>(args)...);
+	}
+
+	template<typename T,
+		typename = enable_if<is_convertible<T, string>::value>::type>
+		void convert(vector<string> &tab, size_t i, T && v)
+	{
+		tab[i] = stringify(forward<T>(v));
+	}
+
+	_LIBCXXABI_HIDDEN _LIBCXXABI_NORETURN void
+		print_abort_message(vector<string> &v);
 }
-#endif
+
+template<typename... Args>
+void abort_message(Args... args)
+{
+	vector<string> val(sizeof...(Args));
+	utility::convert(val, 0, forward<Args>(args)...);
+	print_abort_message(val);
+}
 
 #endif
 
